92bc3755de942e95378accd97a240394c970e346
Enno Hermann <enno.hermann@idiap.ch>
2024-06-26 16:17:53 +0200

    fix(io): apply get_user_data_dir fix from coqui-tts
    Same as in https://github.com/coqui-ai/TTS/pull/2871

--- trainer/io.py
+++ trainer/io.py
@@ -14,9 +14,15 @@ from coqpit import Coqpit
 from trainer.logger import logger
 
 
-def get_user_data_dir(appname):
-    if sys.platform == "win32":
-        import winreg  # pylint: disable=import-outside-toplevel, import-error
+def get_user_data_dir(appname: str) -> Path:
+    TTS_HOME = os.environ.get("TTS_HOME")
+    XDG_DATA_HOME = os.environ.get("XDG_DATA_HOME")
+    if TTS_HOME is not None:
+        ans = Path(TTS_HOME).expanduser().resolve(strict=False)
+    elif XDG_DATA_HOME is not None:
+        ans = Path(XDG_DATA_HOME).expanduser().resolve(strict=False)
+    elif sys.platform == "win32":
+        import winreg  # pylint: disable=import-outside-toplevel
 
         key = winreg.OpenKey(
             winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
